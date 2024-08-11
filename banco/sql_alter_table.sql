0f027ec990dd8f9948ffe08c5ee0da69f9c84f1f
maviifreitass <mariavitoria.freitas@yahoo.com>
2024-08-10 21:44:27 -0300

    Adicionando novos campos tabela produto

---
+++ banco/sql_alter_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE produto ADD COLUMN descricao varchar;
+
+ALTER TABLE produto ADD COLUMN imagem varchar;
+
+ALTER TABLE produto ADD COLUMN cor varchar;
+
+ALTER TABLE produto ADD COLUMN tamanho varchar;
